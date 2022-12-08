91d60f6dd817c5bb4fdeef4aef9f3d3bd3eea7d1
enas48 <enasm4829@gmail.com>
2022-12-08 09:30:15 -0800

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Description: c
+ * Return: 1
+ */
+int main(void)
+{
+	char *M = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	while (*M)
+	{
+	putchar(*M++);
+	}
+	return (1);
+}
