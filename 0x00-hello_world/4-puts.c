200920f0f752dd71dfbed3ebf8f4ee8f250f7a21
enas48 <enasm4829@gmail.com>
2022-12-08 07:14:55 -0800

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-/* hello */
-
 #include<stdio.h>
-
-/* hello puts */
-
+/**
+ * main - Description: c
+ * Return: 0
+*/
 int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle\0";
 
 	puts(Mystr);
-
+/* return 0 */
 	return (0);
 }
