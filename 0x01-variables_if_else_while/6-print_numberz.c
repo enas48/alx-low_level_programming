4aaa45374c7bdf0a6ae730ba6af069de1778bfbf
enas48 <enasm4829@gmail.com>
2022-12-09 12:08:26 -0800

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Description: c
+ * Return: 0
+*/
+int main(void)
+{
+	int d;
+
+	for (d = 0; d <= 9; ++d)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
