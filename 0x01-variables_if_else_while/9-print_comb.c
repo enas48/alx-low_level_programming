7f800c9d0cafc3f7ac66ebdb606a09a79aa64ebc
enas48 <enasm4829@gmail.com>
2022-12-09 12:48:49 -0800

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Description: c
+ * Return: 0
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; ++a)
+	{
+		putchar('0' + a);
+		if (a == 9)
+		{
+			putchar('$');
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
