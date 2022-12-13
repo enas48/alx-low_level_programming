9556f756f7531bade3ebe9bbcb4f8cddfa413beb
enas48 <enasm4829@gmail.com>
2022-12-13 13:07:59 -0800

    a

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Description: alphabet
+ * @c: character
+ * Return: last digit
+*/
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
