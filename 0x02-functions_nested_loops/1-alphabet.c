579dbb1f8aa6188107666a87ad92bbb42b974ddd
enas48 <enasm4829@gmail.com>
2022-12-13 06:01:26 -0800

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - Description: c
+ * Return: 0
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+/**
+ * print_alphabet - Description: c
+ * Return: 0
+*/
+int print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
