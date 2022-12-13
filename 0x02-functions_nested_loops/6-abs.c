197b7247117eff9f5e59f12b1baedf242748014d
enas48 <enasm4829@gmail.com>
2022-12-13 12:56:12 -0800

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Description: alphabet
+ * @c: character
+ * Return: abs of c
+*/
+
+int _abs(int c)
+{
+	return (abs(c));
+}
