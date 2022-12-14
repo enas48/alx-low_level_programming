505e51df28e0407612c4d1f6e619048c6a3294b0
enas48 <enasm4829@gmail.com>
2022-12-14 04:00:16 -0800

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,20 +5,34 @@
  * times_table - Description: prints the 9 times table, starting with 0.
  * Return: prints the 9 times table, starting with 0.
 */
-
 void times_table(void)
 {
-	int i = 0, j;
+	int i = 0, j, m;
 
 	while (i <= 9)
 	{
 		j = 0;
 		while (j <= 9)
 		{
-			_putchar(i * j);
+			m = i * j;
+			if ((m / 10) == 0)
+			{
+				_putchar((m % 10) + '0');
+			}
+			else
+			{
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}
 			j++;
+			if (j == 10)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
 		}
-		_putchar('\n');
+		putchar('\n');
 		i++;
-		}
+	}
 }
