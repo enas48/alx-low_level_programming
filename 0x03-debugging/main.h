55a1b234576fa6e89af91a79b0d34dd7aa87dc34
enas48 <enasm4829@gmail.com>
2022-12-15 16:34:18 +0200

    a

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+#ifndef MAIN_H_
+#define MAIN_H_
 
 void positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int, int);
 void remaining_days(int, int, int);
 
-endif /* MAIN_H */
+#endif /* MAIN_H */
